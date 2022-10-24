6e7a6ad4bd2dd1c73b11a7b5f132194af9547ada
angpace <108838753+angpace@users.noreply.github.com>
2022-10-24 16:45:10 -0400

    added coffee api/render image

--- my-app/src/DrinkContainer.js
+++ my-app/src/DrinkContainer.js
@@ -1,7 +1,34 @@
-function DrinkContainer(){
+import { useEffect, useState } from "react";
+
+function DrinkContainer() {
+    const [coffees, setCoffees] = useState([])
+    const [coffeeIndex, setCoffeIndex] = useState(0)
+
+    useEffect(() => {
+        fetch("https://api.sampleapis.com/coffee/hot")
+        .then(res => res.json())
+        .then(data => setCoffees(data))}, [])
+
+      let random = Math.floor(Math.random() * 3)
+      let randomCoffees = coffees[random]
+
+    const printCoffees = coffees
+    .slice(coffeeIndex, coffeeIndex + 1)
+    .map((coffee) => {
+        return <div key={coffee.description}>
+            <img src={coffee.image}></img>
+            <p>{coffee.title}</p>
+            </div>
+    })
+
+
+
+    // console.log(randomCoffees)
+
     return (
         <div>
-            I am drink container.
+            <h1>Your coffee of the day is:</h1>
+            {printCoffees}
         </div>
     )
 }
