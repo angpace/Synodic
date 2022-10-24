da8b8b00178e2356663c838b5ca2b1992b3cf3d7
madelinedulchin <madeline.dulchin@gmail.com>
2022-10-24 18:32:37 -0400

    todolist on page and data

--- my-app/src/ToDo.js
+++ my-app/src/ToDo.js
@@ -1,6 +1,22 @@
+import React, { useEffect, useState} from "react";
+
 function ToDo() {
+const [toDo, setToDo] = useState([])
+ const todoURL = "http://localhost:3000/tasks"
+useEffect(() => {
+fetch(todoURL)
+.then(res => res.json())
+.then(data => setToDo(data))
+}, [])
+
+const toDoList = toDo.map(task => {
+    return <li>{task.description} </li>
+})
+
     return (
-        <div> I am ToDo </div>
+        <ul>To Do: 
+            {toDoList}
+        </ul>
     )
 }
 
