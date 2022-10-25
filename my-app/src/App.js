b6cfe96b44bcc1d82b11c04044a370e6c7ea0127
madelinedulchin <madeline.dulchin@gmail.com>
2022-10-25 15:13:56 -0400

    routing?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,55 @@ import { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import MainContent from './MainContent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ToDo from './ToDo';
+import Weather from './Weather';
 
 function App() {
+
   const [dayTime, setDayTime] = useState(true)
   const appClass = dayTime ? "App light" : "App dark"
   return (
+    
 
     <div className={appClass}>
       < Header dayTime={dayTime} setDayTime={setDayTime}/>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todo">ToDo</Link>
+            </li>
+            <li>
+              <Link to="/weather">Weather</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/todo">
+            <ToDo />
+          </Route>
+          <Route path="/weather">
+            <Weather />
+          </Route>
+          <Route path="/">
+            <MainContent />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
       <MainContent dayTime={dayTime}/>
     </div>
   );
