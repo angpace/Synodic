08baef5eb902755a49df08479139636ab0ce0aba
Keshav Bhati <keshavbhati2000@gmail.com>
2022-10-24 23:13:00 -0400

    fetch daily weather from API

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -1,8 +1,30 @@
-function Weather(){
-    return (
-        <div>I am Weather
-        </div>
-    )
+import { useEffect, useState } from "react"
+
+function Weather() {
+    const [todaysWeather, setTodaysWeather] = useState({})
+
+    useEffect(() => {
+        let date = new Date().toJSON().slice(0, 10);
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,weathercode&daily=sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&start_date=${date}&end_date=${date}`)
+            .then(res => res.json())
+            .then(data => setTodaysWeather(data))
+    }, [])
+
+    if (Object.keys(todaysWeather).length > 0) {
+        const { sunrise, sunset } = todaysWeather.daily
+        const { temperature_2m, weathercode } = todaysWeather.hourly
+        console.log(todaysWeather)
+        return (
+            <>
+                <div>Click Here for todays Weather</div>
+                <div>{sunrise}</div>
+                <div>{sunset}</div>
+            </>
+        )
+    }
+    else {
+        return <div>Loading ...</div>
+    }
 }
 
 export default Weather;
