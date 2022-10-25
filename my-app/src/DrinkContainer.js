786ee5c5684bea6a7108a89c1e7be71dc5cd2a63
angpace <108838753+angpace@users.noreply.github.com>
2022-10-25 11:10:36 -0400

    Random coffeegit add .

--- my-app/src/DrinkContainer.js
+++ my-app/src/DrinkContainer.js
@@ -1,57 +1,59 @@
 import { useEffect, useState } from "react";
 
 function DrinkContainer() {
-    const [coffees, setCoffees] = useState([])
-    const [coffeeIndex, setCoffeIndex] = useState(0)
-    const [cocktails, setCocktails] = useState([])
+  const [coffees, setCoffees] = useState([])
+  const [cocktails, setCocktails] = useState([])
 
-    useEffect(() => {
-        fetch("https://api.sampleapis.com/coffee/hot")
-        .then(res => res.json())
-        .then(data => setCoffees(data))}, [])
+  //Coffee Api
+  useEffect(() => {
+    fetch("https://api.sampleapis.com/coffee/hot")
+      .then(res => res.json())
+      .then(data => setCoffees(data))
+  }, [])
+  //Generates random coffees
+  let random = (Math.floor(Math.random() * 20) + 1)
+  console.log(random)
 
-      let random = Math.floor(Math.random() * 3)
-      let randomCoffees = coffees[random]
-
-    const printCoffees = coffees
-    .slice(coffeeIndex, coffeeIndex + 1)
+  const printCoffees = coffees
     .map((coffee) => {
+      if (coffee.id === random)
         return <div key={coffee.description}>
-            <img src={coffee.image}></img>
-            <p>{coffee.title}</p>
-            </div>
+          <img style={{ width: "300px" }} src={coffee.image}></img>
+          <p>{coffee.title}</p>
+          <p>{coffee.description}</p>
+        </div>
     })
 
 
-    useEffect(() => {
-      fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-      .then( res => res.json())
+  useEffect(() => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+      .then(res => res.json())
       .then(data => setCocktails(data.drinks))
-    }, [])
-  
-    const renderCocktails = cocktails.map(cocktail => {
-      return(
-        <div key={cocktail.idDrink}>
-          <h2>{cocktail.strDrink}</h2>
-          <img
-            src={cocktail.strDrinkThumb} alt={cocktail.strDrink}
-            style={{ width: "300px"}} 
-          />
-          <p>{cocktail.strInstructions}</p>
-        </div>
-      )
-    })
+  }, [])
 
-    // console.log(randomCoffees)
-
+  const renderCocktails = cocktails.map(cocktail => {
     return (
-        <div>
-            <h1>Your coffee of the day is:</h1>
-            {printCoffees}
-            <h1>Cocktails?</h1>
-            {renderCocktails}
-        </div>
+      <div key={cocktail.idDrink}>
+        <h2>{cocktail.strDrink}</h2>
+        <img
+          src={cocktail.strDrinkThumb} alt={cocktail.strDrink}
+          style={{ width: "300px" }}
+        />
+        <p>{cocktail.strInstructions}</p>
+      </div>
     )
+  })
+
+  // console.log(randomCoffees)
+
+  return (
+    <div>
+      <h3>Suggested Coffee of the day:</h3>
+      {printCoffees}
+      <h3>Suggested Cocktail of the night:</h3>
+      {renderCocktails}
+    </div>
+  )
 }
 
 export default DrinkContainer;
