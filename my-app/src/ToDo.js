110d78df3856fd2ae92ea64c0302bb564e5615d3
madelinedulchin <madeline.dulchin@gmail.com>
2022-10-25 10:37:38 -0400

    adding to do form

--- my-app/src/ToDo.js
+++ my-app/src/ToDo.js
@@ -10,13 +10,19 @@ fetch(todoURL)
 }, [])
 
 const toDoList = toDo.map(task => {
-    return <li>{task.description} </li>
+    return <li key={task.id}>{task.description}</li>
 })
 
     return (
+        <div>
         <ul>To Do: 
             {toDoList}
         </ul>
+        <form>
+            <input type="text" name="description" placeholder="Add New To Do!"></input>
+            <button type="submit">Submit</button>
+        </form>
+        </div>
     )
 }
 
