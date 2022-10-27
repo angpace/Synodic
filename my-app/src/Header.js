fc02175ea9162df199486e44707c32d10f1c878f
madelinedulchin <madeline.dulchin@gmail.com>
2022-10-26 21:17:40 -0400

    container changes

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 const Head = styled.h1`
 font-size: 4em;
 text-align: left;
-color: palevioletred
+color: palevioletred;
+margin-left: 1%;
 `
 
 const StyledLink = styled(Link)`
@@ -17,24 +18,20 @@ transform: scale(1.5);
 const Button = styled.button`
 display: inline-block;
 color: palevioletred;
-font-size: .45em;
+font-size: .6em;
 margin: .25em;
 padding: 0.25em;
 border: 0px solid palevioletred;
 border-radius: .5px;
 display: block;
 background-color: transparent;
+
 :hover{
 margin-top: 2px;
-/* transform: scale(1.5); */
+
 }
 `
-/* const StyledLink = styled(Link)`
-  color: Blue;
-  text-decoration: none;
-  margin: 1rem;
-  position: relative;
-`; */
+
 function Header({ dayTime, setDayTime }) {
 
 
